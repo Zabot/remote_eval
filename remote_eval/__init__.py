aeb9e5f51465b20e96c2848e5e599113df00d1ec
Zach Anderson <zach.inbox@gmail.com>
2020-07-30 16:22:43 -0500

    Use POST route for requests

--- remote_eval/__init__.py
+++ remote_eval/__init__.py
@@ -4,7 +4,8 @@ from remote_eval.remote_eval import handle_message
 
 app = flask.Flask(__name__)
 
-@app.route('/remote_eval')
+@app.route('/remote_eval', methods=['POST'])
 def remote_eval_endpoint():
-    return flask.jsonify(**handle_message(flask.request.json))
+    response = handle_message(flask.request.json)
+    return flask.jsonify(response)
 
